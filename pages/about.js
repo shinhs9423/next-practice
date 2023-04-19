123ca96d39e37948e5b5957116b72cb5f722379d
hanseunghyeon <shinhs9423@naver.com>
2023-04-19 23:55:13 +0900

    - pages create file & dir

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+/**
+ * 제작 : hanseunghyeon
+ * 날짜 : 2023/04/19
+ * 내용 : about page
+ */
+
+function About() {
+  return (
+    <>
+      <div>About</div>
+    </>
+  );
+}
+
+export default About;
---
+++ pages/view/[id].js
@@ -0,0 +1,17 @@
+/**
+ * 제작 : hanseunghyeon
+ * 날짜 : 2023/04/19
+ * 내용 : view page
+ */
+
+function View() {
+  return (
+    <>
+      <div>
+        View
+      </div>
+    </>
+  );
+}
+
+export default View;
